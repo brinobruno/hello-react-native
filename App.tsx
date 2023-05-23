af7cc9b4da6417d6ecefc9c4aed0f5d2e5b5972d
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-05-23 18:52:28 -0300

    chore: Add more elements and styles

--- App.tsx
+++ App.tsx
@@ -4,12 +4,25 @@ export default function App() {
   return (
     <View style={{
       flex: 1,
-      backgroundColor: '#333333',
+      backgroundColor: '#111111',
+      padding: 24
     }}>
-      <Text style={{color: '#d3d3d3'}}>
-        Open up App.tsx
+      <Text style={{
+        color: '#FDFCFE',
+        fontSize: 24,
+        fontWeight: '600'
+      }}
+      >
+        Event name
       </Text>
-      <Text>to start working on your app!</Text>
+
+      <Text style={{
+        color: '#a0a0a0',
+        fontSize: 16,
+      }}
+      >
+        Friday, May 23rd, 2023.
+      </Text>
     </View>
   );
 }
