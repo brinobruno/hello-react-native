49a0ff6336b44f67867ad8b80ac7346d56455927
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-05-24 20:46:20 -0300

    chore: Lint fix

--- App.tsx
+++ App.tsx
@@ -1,28 +1,32 @@
-import {  Text, View } from 'react-native';
+import { Text, View } from 'react-native'
 
 export default function App() {
   return (
-    <View style={{
-      flex: 1,
-      backgroundColor: '#111111',
-      padding: 24
-    }}>
-      <Text style={{
-        color: '#FDFCFE',
-        fontSize: 24,
-        fontWeight: '600'
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: '#111111',
+        padding: 24,
       }}
+    >
+      <Text
+        style={{
+          color: '#FDFCFE',
+          fontSize: 24,
+          fontWeight: '600',
+        }}
       >
         Event name
       </Text>
 
-      <Text style={{
-        color: '#a0a0a0',
-        fontSize: 16,
-      }}
+      <Text
+        style={{
+          color: '#a0a0a0',
+          fontSize: 16,
+        }}
       >
         Friday, May 23rd, 2023.
       </Text>
     </View>
-  );
+  )
 }
