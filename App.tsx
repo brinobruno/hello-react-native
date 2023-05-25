39aa0838475ed91f848c037cd147099587bcb611
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-05-24 21:14:41 -0300

    chore: Render Home component on App

--- App.tsx
+++ App.tsx
@@ -1,28 +1,9 @@
-import { StyleSheet, Text, View } from 'react-native'
+import Home from './src/screens/Home'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Event name</Text>
-
-      <Text style={styles.description}>Friday, May 23rd, 2023.</Text>
-    </View>
+    <>
+      <Home />
+    </>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#111111',
-    padding: 24,
-  },
-  title: {
-    color: '#FDFCFE',
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  description: {
-    color: '#a0a0a0',
-    fontSize: 16,
-  },
-})
