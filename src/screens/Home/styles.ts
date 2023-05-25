1eb84824e3e009db7937fd7dd1b9ff10c24802cb
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-05-24 21:14:25 -0300

    feat: Add Home component

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#111111',
+    padding: 24,
+  },
+  title: {
+    color: '#FDFCFE',
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  description: {
+    color: '#a0a0a0',
+    fontSize: 16,
+  },
+})
